a528e973ace7b2c45175853c030b7e1745a9a4cb
ldphat99 <ldphat99@gmail.com>
2023-10-27 07:58:01 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page d-flex justify-content-center align-items-center" style="height: 80vh;">
+        <p>
+            Oops, không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ.</router-link>
+        </p>
+    </div>
+</template>
